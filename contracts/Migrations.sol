cb314b62c56f6edf8726016a2486c11b64121c8c
Edu Lazaro <edu@kenodo.com>
2024-04-24 13:05:25 +0200

    Allocate token balances

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,18 +1,24 @@
-pragma solidity >=0.4.21 <0.7.0;
+// SPDX-License-Identifier: MIT
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+pragma solidity ^0.8.25;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+contract Migrations
+{
+    address public owner;
+    uint public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+    constructor()
+    {
+        owner = msg.sender;
+    }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint completed) public restricted
+    {
+        last_completed_migration = completed;
+    }
+
+    modifier restricted()
+    {
+        if (msg.sender == owner) _;
+    }
 }
--- contracts/mocks/Zrx.sol
+++ contracts/mocks/Zrx.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.25;
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Zrx is ERC20
 {
